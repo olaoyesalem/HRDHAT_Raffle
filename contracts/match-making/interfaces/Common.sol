02e1b87460f89427d46c16737839951c426894e3
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-03-26 07:15:17 +0000

    Update Common.sol

--- contracts/match-making/interfaces/Common.sol
+++ contracts/match-making/interfaces/Common.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IBank.sol";
 
+
 interface Common {
   error InvalidBankAddress();
   error YouShouldAtLeastHaveARelative();
